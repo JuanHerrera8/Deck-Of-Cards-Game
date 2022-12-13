8ad293928b657f7ab1fc78d3465f3b7651539706
JuanPC <herrera.francojd@gmail.com>
2022-12-13 15:37:51 -0500

    first commit

--- src/context/GameProvider.jsx
+++ src/context/GameProvider.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DeckOfCardsAPI from '../services/deckofcardsapi';
 import GameContext from './GameContext';
 
 const GameProvider = ({ children }) => {
 	const [idGame, setIdGame] = useState(null);
-	const [win, setWin] = useState(false);
+	const [, /* win */ setWin] = useState(false);
 	const [showToast, setShowToast] = useState(false);
 	const [winName, setWinName] = useState('');
+
 	const [playerOne, setPlayerOne] = useState({
 		name: '',
 		cards: [],
@@ -16,29 +17,85 @@ const GameProvider = ({ children }) => {
 		cards: [],
 	});
 
+	const getSameValueCards = player => {
+		const cards = player.cards;
+		const sameValueCards = [];
+
+		cards.forEach((card, index) => {
+			const findCard = cards.find(
+				(cardFind, indexFind) =>
+					cardFind.value === card.value && index !== indexFind
+			);
+
+			if (findCard) {
+				sameValueCards.push(findCard);
+			}
+		});
+
+		return sameValueCards;
+	};
+
+	const findCardPlayerOne = () => {
+		if (getSameValueCards(playerOne).length === 10) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const findCardPlayerTwo = () => {
+		if (getSameValueCards(playerTwo).length === 10) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
 	const playGame = async () => {
 		setIdGame(await DeckOfCardsAPI.getIdGame());
 	};
 
 	const requestCards = async () => {
 		const cards = await DeckOfCardsAPI.getCards(idGame);
+
 		setPlayerOne({ ...playerOne, cards: [...playerOne.cards, cards[0]] });
 		setPlayerTwo({ ...playerTwo, cards: [...playerTwo.cards, cards[1]] });
 
-		const findCardPlayerOne = playerOne.cards.find(
-			card => card.value === cards[0].value
-		);
-
-		const findCardPlayerTwo = playerTwo.cards.find(
-			card => card.value === cards[1].value
-		);
-
-		if (findCardPlayerOne || findCardPlayerTwo) {
+		if (findCardPlayerOne()) {
+			console.log('Ganador: ', playerOne.name);
 			setWin(true);
 			setShowToast(true);
-			setWinName(findCardPlayerOne ? playerOne.name : playerTwo.name);
+			setWinName(playerOne.name);
+		}
+
+		if (findCardPlayerTwo()) {
+			console.log('Ganador: ', playerTwo.name);
+			setWin(true);
+			setShowToast(true);
+			setWinName(playerTwo.name);
 		}
 	};
+
+	useEffect(() => {
+		DeckOfCardsAPI.getDeck(idGame)
+			.then(res => {
+				const cards = res;
+				const cardsPlayerOne = [];
+				const cardsPlayerTwo = [];
+
+				cards.forEach((card, index) => {
+					if (index % 2 === 0) {
+						cardsPlayerOne.push(card);
+					} else {
+						cardsPlayerTwo.push(card);
+					}
+				});
+
+				setPlayerOne({ ...playerOne, cards: cardsPlayerOne });
+				setPlayerTwo({ ...playerTwo, cards: cardsPlayerTwo });
+			})
+			.catch(err => console.log(err));
+	}, [idGame]);
 
 	return (
 		<GameContext.Provider
@@ -52,6 +109,7 @@ const GameProvider = ({ children }) => {
 				showToast,
 				setShowToast,
 				winName,
+				idGame,
 			}}
 		>
 			{children}
--- src/services/deckofcardsapi.jsx
+++ src/services/deckofcardsapi.jsx
@@ -5,16 +5,56 @@ const getIdGame = async () => {
 	return data?.deck_id;
 };
 
+const valueCard = card => {
+	let newCard = card;
+
+	if (card === 'JACK') {
+		newCard = '11';
+	} else if (card === 'QUEEN') {
+		newCard = '12';
+	} else if (card === 'KING') {
+		newCard = '13';
+	} else if (card === 'ACE') {
+		newCard = '1';
+	}
+
+	return newCard;
+};
+
 const getCards = async deckId => {
 	const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
 	const res = await fetch(url);
 	const data = await res.json();
-	return data?.cards;
+
+	return [
+		{
+			...data.cards[0],
+			newValue: valueCard(data.cards[0].value),
+		},
+		{
+			...data.cards[1],
+			newValue: valueCard(data.cards[1].value),
+		},
+	];
+};
+
+const getDeck = async deckId => {
+	const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=20`;
+	const res = await fetch(url);
+	const data = await res.json();
+
+	return (
+		data?.cards?.map(card => ({
+			...card,
+			newValue: valueCard(card.value),
+		})) || []
+	);
 };
 
 const DeckOfCardsAPI = {
 	getIdGame,
 	getCards,
+	getDeck,
 };
 
 export default DeckOfCardsAPI;
